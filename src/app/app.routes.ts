f78474b89bb088cb746c6252573a81fe4c0bbab4
BA ILIAS <bailias621@gmail.com>
2025-07-16 08:59:53 +0000

    Feat maintenance page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
+    /*{
         path:'',
         loadChildren : ()=> import('../app/modules/landing/home/home.routes'),
+    },*/
+    {
+        path:'',
+        loadChildren : ()=> import('../app/modules/pages/info-page/infos-page.routes'),
     },
     {
         path:'faq',
